2c6618f7c104adaef262d5e4ed8c6c8144663e07
aldod400 <aldodelghonemy400@gmail.com>
2025-12-07 15:58:55 +0200

    fix: correct HttpExceptionFilter class name and improve error handling; add empty constructor to UsersController

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
-import { HttpExceptionsFilter } from './common/filters/http-exception.filter';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { useContainer } from 'class-validator';
 
 async function bootstrap() {
@@ -17,7 +17,7 @@ async function bootstrap() {
   }));
 
   app.useGlobalInterceptors(new ResponseInterceptor());
-  app.useGlobalFilters(new HttpExceptionsFilter());
+  app.useGlobalFilters(new HttpExceptionFilter());
   
   await app.listen(process.env.PORT ?? 3000);
 }
